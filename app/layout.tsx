a726bbdf43201ed8fbc78d54b6eab34446602130
0xd3bs <dblancove@gmail.com>
2025-09-06 17:33:19 -0300

    fix: layout issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,29 @@ import "@coinbase/onchainkit/styles.css";
 import "./globals.css";
 import { Providers } from "./providers";
 
-export const metadata: Metadata = {
-  title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-    openGraph: {
-      title: process.env.NEXT_PUBLIC_APP_SUBTITLE,
-      description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
-      images: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
-      url: process.env.NEXT_PUBLIC_URL,
-      siteName: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME
+export async function generateMetadata(): Promise<Metadata> {
+  const URL = process.env.NEXT_PUBLIC_URL;
+  return {
+    title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+    description: "Your Mini App description here",
+    other: {
+      "fc:frame": JSON.stringify({
+        version: "next",
+        imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+        button: {
+          title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
+          action: {
+            type: "launch_frame",
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            url: URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE,
+            splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+          },
+        },
+      }),
     },
-};
+  };
+}
 
 export default async function RootLayout({
   children,
