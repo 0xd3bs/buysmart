020a69b4ef10f9fd9deb702c0e152006fe700edc
0xd3bs <dblancove@gmail.com>
2025-09-09 21:30:42 -0300

    fix: update app description and add new og_embed_image

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export async function generateMetadata(): Promise<Metadata> {
   const URL = process.env.NEXT_PUBLIC_URL;
   return {
     title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-    description: "Your Mini App description here",
+    description: process.env.NEXT_PUBLIC_APP_SUBTITLE,
     other: {
       "fc:frame": JSON.stringify({
         version: "next",
